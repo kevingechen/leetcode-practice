78b2ca3d1abc4b521793ff44e61402e6ccd27880
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-02-08 02:27:18 +0000

    feat: evaluate RPN

---
+++ stack/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            match token:
+                case '+':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(op1 + op2)
+                case '-':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(op1 - op2)
+                case '*':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(op1 * op2)
+                case '/':
+                    op2, op1 = stack.pop(), stack.pop()
+                    stack.append(int(op1 / op2))
+                case _:
+                    stack.append(int(token))
+        return stack.pop()
