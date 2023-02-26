bb1c16aca6c596775554a353fbbe2e572f8d8d14
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-02-26 15:45:45 +0000

    draft: update insert and geRandom

--- hashmap/380_Insert_and_GetRandom.py
+++ hashmap/380_Insert_and_GetRandom.py
@@ -1,31 +1,46 @@
+class DlNode:
+
+    def __init__(self, val, prev = None, next = None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+
 class RandomizedSet:
 
     def __init__(self):
-        self.size = 0
-        self.data_map = {}
-        self.data_arr = []
+        self.cursor = None
+        self.data_node_map = {}
 
     def insert(self, val: int) -> bool:
-        if self.data_map.get(val, False):
+        if self.data_node_map.get(val, None) is not None:
             return False
-        self.data_arr.append(val)
-        self.data_map[val] = True
-        self.size += 1
+        if self.cursor is None:
+            new_node = DlNode(val, None, None)
+            new_node.prev = new_node
+            new_node.next = new_node
+            self.cursor = new_node
+            self.data_node_map[val] = new_node
+        else:
+            next_node = self.cursor.next
+            new_node = DlNode(val, self.cursor, next_node)
+            self.cursor.next = new_node
+            next_node.prev = new_node
+            self.data_node_map[val] = new_node
         return True
 
     def remove(self, val: int) -> bool:
-        if self.data_map.get(val, False):
-            self.data_arr.append(val)
-            self.data_map[val] = False
-            self.size -= 1
-        return False
+        if self.data_node_map.get(val, None) is None:
+            return False
+        self.data_node_map[val].prev.next = self.data_node_map[val].next
+        self.data_node_map[val].next.prev = self.data_node_map[val].prev
+        if self.cursor == self.data_node_map[val]:
+            self.cursor = self.data_node_map[val].next
+            if self.cursor == self.data_node_map[val]:
+                self.cursor = None
+        self.data_node_map.pop(val)
+        return True
 
     def getRandom(self) -> int:
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        self.cursor = self.cursor.next
+        return self.cursor.val
