d6056d7f0df7adbd33b7322f85cc52c439bfd503
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-03-01 01:07:44 +0000

    feat: optimize time for top k frequent elements

--- heap/347_TopK_Frequent_Elements.py
+++ heap/347_TopK_Frequent_Elements.py
@@ -1,9 +1,26 @@
 class Solution:
-
-    def topKFrequentElemnts(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         num_map = {}
         for num in nums:
             num_map.setdefault(num, 0)
             num_map[num] += 1
-        return [item[0] for item in sorted(num_map.items(), key = lambda item: item[1], reverse = True)[:k]]
-
+        num_counts = list(num_map.items())
+        print(num_counts)
+        self.arrangeKthFrequentItem(num_counts, k, 0, len(num_counts)-1)
+        return [val for (val, count) in num_counts[:k]]
+    
+    def arrangeKthFrequentItem(self, num_counts: List[Tuple[int, int]], k: int, left: int, right: int) -> Tuple[int, int]:
+        pivot = num_counts[left]
+        pivot_idx = left
+        for i in range(left+1, right+1):
+            if num_counts[i][1] >= pivot[1]:
+                pivot_idx += 1
+                num_counts[pivot_idx],num_counts[i] = num_counts[i],num_counts[pivot_idx] 
+        num_counts[left],num_counts[pivot_idx] = num_counts[pivot_idx],pivot
+        pivot_k = pivot_idx - left + 1
+        if pivot_k == k:
+            return
+        elif pivot_k < k:
+            self.arrangeKthFrequentItem(num_counts, k - pivot_k, pivot_idx + 1, right)
+        else:
+            self.arrangeKthFrequentItem(num_counts, k, left, pivot_idx - 1)
