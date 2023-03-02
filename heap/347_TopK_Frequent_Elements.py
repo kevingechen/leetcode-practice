105f5eb08f4cecf46f7d4fbb7711ab230753d7a9
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-03-02 01:18:47 +0000

    refactor: polish top k frequent impl

--- heap/347_TopK_Frequent_Elements.py
+++ heap/347_TopK_Frequent_Elements.py
@@ -8,19 +8,20 @@ class Solution:
         print(num_counts)
         self.arrangeKthFrequentItem(num_counts, k, 0, len(num_counts)-1)
         return [val for (val, count) in num_counts[:k]]
-    
-    def arrangeKthFrequentItem(self, num_counts: List[Tuple[int, int]], k: int, left: int, right: int) -> Tuple[int, int]:
+
+   def arrangeKthFrequentItem(self, num_counts: List[Tuple[int, int]], k: int, left: int, right: int) -> Tuple[int, int]:
         pivot = num_counts[left]
         pivot_idx = left
         for i in range(left+1, right+1):
             if num_counts[i][1] >= pivot[1]:
+                num_counts[pivot_idx] = num_counts[i] 
                 pivot_idx += 1
-                num_counts[pivot_idx],num_counts[i] = num_counts[i],num_counts[pivot_idx] 
-        num_counts[left],num_counts[pivot_idx] = num_counts[pivot_idx],pivot
+                num_counts[i] = num_counts[pivot_idx]
+        num_counts[pivot_idx] = pivot
         pivot_k = pivot_idx - left + 1
         if pivot_k == k:
             return
         elif pivot_k < k:
             self.arrangeKthFrequentItem(num_counts, k - pivot_k, pivot_idx + 1, right)
         else:
-            self.arrangeKthFrequentItem(num_counts, k, left, pivot_idx - 1)
+            self.arrangeKthFrequentItem(num_counts, k, left, pivot_idx - 1) 
