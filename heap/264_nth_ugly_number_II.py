317b18c547b6be984d5bd0731485bf3d337ffa5c
Ge CHEN (Kevin) <kevin.ge.chen@gmail.com>
2023-03-11 03:36:18 +0000

    refactor: polish ugly number II

--- heap/264_nth_ugly_number_II.py
+++ heap/264_nth_ugly_number_II.py
@@ -1,29 +1,36 @@
+class UglyFactor:
+    def __init__(self, ugly_set, ugly_seq, seq_idx, factor):
+        self.ugly_set = ugly_set
+        self.ugly_seq = ugly_seq
+        self.seq_idx = seq_idx
+        self.factor = factor
+    
+    def incSeq(self):
+        self.seq_idx += 1
+
+    def getNextUgly(self):
+        next_ugly = self.ugly_seq[self.seq_idx] * self.factor
+        while next_ugly in self.ugly_set:
+            self.incSeq()
+            next_ugly = self.ugly_seq[self.seq_idx] * self.factor
+        return next_ugly
+
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         ugly_set = {1: True}
         ugly_seq = [1]
         size = 1
-        two_base, three_base, five_base = 0, 0, 0
+        ugly_factors = [UglyFactor(ugly_set, ugly_seq, 0, 2), UglyFactor(ugly_set, ugly_seq, 0, 3), UglyFactor(ugly_set, ugly_seq, 0, 5)]
         while size < n:
-            next_ugly_by_two = ugly_seq[two_base] * 2
-            while next_ugly_by_two in ugly_set:
-                two_base += 1
-                next_ugly_by_two = ugly_seq[two_base] * 2
-            next_ugly_by_three = ugly_seq[three_base] * 3
-            while next_ugly_by_three in ugly_set:
-                three_base += 1
-                next_ugly_by_three = ugly_seq[three_base] * 3
-            next_ugly_by_five = ugly_seq[five_base] * 5
-            while next_ugly_by_five in ugly_set:
-                five_base += 1
-                next_ugly_by_five = ugly_seq[five_base] * 5
-            next_ugly = min(next_ugly_by_two, next_ugly_by_three, next_ugly_by_five)
-            if next_ugly == next_ugly_by_two:
-                two_base += 1
-            if next_ugly == next_ugly_by_three:
-                three_base += 1
-            if next_ugly == next_ugly_by_five:
-                five_base += 1
+            next_uglys = []
+            for ugly_factor in ugly_factors:
+                next_uglys.append(ugly_factor.getNextUgly())
+            min_idx = 0
+            next_ugly = next_uglys[0]
+            for i in range(1,3):
+                if next_ugly > next_uglys[i]:
+                    next_ugly = next_uglys[i]
+                    min_idx = i
             ugly_set[next_ugly] = True
             ugly_seq.append(next_ugly)
             size += 1
